a3afcec0e5d36480f3de88082d07f4e515d4bda1
MarikaKonturova <mashakonturova@gmail.com>
2023-02-10 10:43:32 +0300

    add mansory

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Bellota_Text } from "@next/font/google";
 
+const bellota = Bellota_Text({
+  subsets: ["latin"],
+  weight: ["300", "400", "700"],
+  variable: "--bellota-font",
+});
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`${bellota.variable} font-sans`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
